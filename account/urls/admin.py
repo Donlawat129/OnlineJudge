e002763939fcb649418437fbcae1e8894b78603e
Donlawat Sasawattecha <donlawat.sas@gmail.com>
2025-09-06 22:08:49 +0700

    Update admin.py

--- account/urls/admin.py
+++ account/urls/admin.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 
 from ..views.admin import UserAdminAPI, GenerateUserAPI
+# [ADD]
+from ..views.admin import AdminGroupAPI, AdminAssignUsersToGroupAPI
 
 urlpatterns = [
     url(r"^user/?$", UserAdminAPI.as_view(), name="user_admin_api"),
     url(r"^generate_user/?$", GenerateUserAPI.as_view(), name="generate_user_api"),
+]
+
+# [ADD]
+urlpatterns += [
+    url(r"^groups/?$", AdminGroupAPI.as_view(), name="admin_group_api"),
+    url(r"^groups/assign/?$", AdminAssignUsersToGroupAPI.as_view(), name="admin_group_assign_api"),
 ]
