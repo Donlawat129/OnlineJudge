fc2a61b54c6f7c556d5c9d5b0ee3e41c5c34ce2b
Donlawat Sasawattecha <donlawat.sas@gmail.com>
2025-09-07 17:34:25 +0700

    Update admin.py

--- account/urls/admin.py
+++ account/urls/admin.py
@@ -1,19 +1,14 @@
 from django.conf.urls import url
-
-from ..views.admin import UserAdminAPI, GenerateUserAPI
-# [ADD]
-from ..views.group import (
-    GroupAdminAPI, AssignUsersToGroupAPI,
-    RemoveUsersFromGroupAPI, ClearUsersGroupsAPI
+from ..views.admin import (
+    UserAdminAPI, GenerateUserAPI,
+    GroupAPI, GroupAssignAPI, GroupRemoveAPI, GroupClearAPI
 )
 
 urlpatterns = [
     url(r"^user/?$", UserAdminAPI.as_view(), name="user_admin_api"),
     url(r"^generate_user/?$", GenerateUserAPI.as_view(), name="generate_user_api"),
-
-    # [ADD] group endpoints
-    url(r"^groups/?$", GroupAdminAPI.as_view(), name="group_admin_api"),
-    url(r"^groups/assign/?$", AssignUsersToGroupAPI.as_view(), name="group_assign_api"),
-    url(r"^groups/remove/?$", RemoveUsersFromGroupAPI.as_view(), name="group_remove_api"),
-    url(r"^groups/clear/?$", ClearUsersGroupsAPI.as_view(), name="group_clear_api"),
+    url(r"^groups/?$", GroupAPI.as_view(), name="group_list_api"),
+    url(r"^groups/assign/?$", GroupAssignAPI.as_view(), name="group_assign_api"),
+    url(r"^groups/remove/?$", GroupRemoveAPI.as_view(), name="group_remove_api"),
+    url(r"^groups/clear/?$", GroupClearAPI.as_view(), name="group_clear_api"),
 ]
