d7de2a1362445d181e15ffc6e3a0d19160d6cd1e
Donlawat Sasawattecha <donlawat.sas@gmail.com>
2025-09-08 19:49:29 +0700

    Update utils.py

--- account/utils.py
+++ account/utils.py
@@ -12,10 +12,16 @@ def user_groups_qs(user):
         return Group.objects.none()
     return Group.objects.filter(usergroup__user=user)
 
-def filter_by_user_groups(qs, user, *, visible_field='visible', m2m_field='groups'):
+def filter_users_in_same_groups(qs, user):
+    """
+    กรอง queryset ของ User ให้เหลือเฉพาะคนที่ 'แชร์อย่างน้อยหนึ่งกลุ่ม' กับ user ปัจจุบัน
+    - แอดมิน/ซุปเปอร์แอดมิน: เห็นทั้งหมด
+    - ถ้า user ไม่อยู่ในกลุ่มไหนเลย: เห็นเฉพาะผู้ใช้ที่ 'ไม่มี' กลุ่ม (usergroup.isnull=True)
+    """
     if is_admin_or_super(user):
         return qs
     ugs = user_groups_qs(user)
-    return qs.filter(**{visible_field: True}).filter(
-        Q(**{f'{m2m_field}__isnull': True}) | Q(**{f'{m2m_field}__in': ugs})
-    ).distinct()
+    if ugs.exists():
+        return qs.filter(usergroup__group__in=ugs).distinct()
+    else:
+        return qs.filter(usergroup__isnull=True).distinct()
